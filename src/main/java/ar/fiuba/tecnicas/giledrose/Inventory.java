de7fab25f0c3e1edb98fa1a66b274ffce825f0f7
Martin Lucero <luceromartinandres89630@gmail.com>
2013-09-18 22:59:58 -0300

    Refactoring: Introduce explaining method. Se reemplazaron las comparaciones dentro de los if() por métodos que retornan boolean. Se hicieron métodos para el incremento y/o decremento de los atributos quality y sellin. Sin embargo esto genera otros smells en Inventory, como Large Class o Feature Envy. Esto se solucionaría fácilmente si se pudiese migrar estos métodos a la clase Item (pero no se puede por las restricciones del enunciado).

--- src/main/java/ar/fiuba/tecnicas/giledrose/Inventory.java
+++ src/main/java/ar/fiuba/tecnicas/giledrose/Inventory.java
@@ -1,75 +1,136 @@
 package ar.fiuba.tecnicas.giledrose;
 
 public class Inventory {
-    private Item[] items;
+	/* TODO: ver si puedo acomodar estas definiciones en otro lugar más
+	 * apropiado */
+	private static final String NAME_BRIE = "Aged Brie";
+	private static final String NAME_SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final String NAME_BACKSTAGE =
+			"Backstage passes to a TAFKAL80ETC concert";
 
-    public Inventory(Item[] items) {
-        super();
-        this.items = items;
-    }
+	private Item[] items;
 
-    public Inventory() {
-        super();
-        items = new Item[]{
-                new Item("+5 Dexterity Vest", 10, 20),
-                new Item("Aged Brie", 2, 0),
-                new Item("Elixir of the Mongoose", 5, 7),
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Conjured Mana Cake", 3, 6)
-        };
+	public Inventory(Item[] items) {
+		super();
+		this.items = items;
+	}
 
-    }
+	public Inventory() {
+		super();
+		items =
+				new Item[] {
+						new Item("+5 Dexterity Vest", 10, 20),
+						new Item("Aged Brie", 2, 0),
+						new Item("Elixir of the Mongoose", 5, 7),
+						new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+						new Item("Backstage passes to a TAFKAL80ETC concert",
+								15, 20), new Item("Conjured Mana Cake", 3, 6) };
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].getName() != "Aged Brie" && items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].getQuality() > 0) {
-                    if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-                        items[i].setQuality(items[i].getQuality() - 1);
-                    }
-                }
-            } else {
-                if (items[i].getQuality() < 50) {
-                    items[i].setQuality(items[i].getQuality() + 1);
+	}
 
-                    if (items[i].getName() == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].getSellIn() < 11) {
-                            if (items[i].getQuality() < 50) {
-                                items[i].setQuality(items[i].getQuality() + 1);
-                            }
-                        }
+	public void updateQuality() {
+		for (int i = 0; i < items.length; i++) {
+			Item itemToUpdate = items[i];
+			if (!isItemAgedBrie(itemToUpdate)
+					&& !isItemConcertPasses(itemToUpdate)) {
+				if (isItemQualityGreaterThanZero(itemToUpdate)) {
+					if (!isItemSulfuras(itemToUpdate)) {
+						decrementItemQualityByOne(itemToUpdate);
+					}
+				}
+			}
+			else {
+				if (isItemQualityLessThanFifty(itemToUpdate)) {
+					incrementItemQualityByOne(itemToUpdate);
 
-                        if (items[i].getSellIn() < 6) {
-                            if (items[i].getQuality() < 50) {
-                                items[i].setQuality(items[i].getQuality() + 1);
-                            }
-                        }
-                    }
-                }
-            }
+					if (isItemConcertPasses(itemToUpdate)) {
+						if (isItemSellInLessThanEleven(itemToUpdate)) {
+							if (isItemQualityLessThanFifty(itemToUpdate)) {
+								incrementItemQualityByOne(itemToUpdate);
+							}
+						}
 
-            if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-                items[i].setSellIn(items[i].getSellIn() - 1);
-            }
+						if (isItemSellInLessThanSix(itemToUpdate)) {
+							if (isItemQualityLessThanFifty(itemToUpdate)) {
+								incrementItemQualityByOne(itemToUpdate);
+							}
+						}
+					}
+				}
+			}
 
-            if (items[i].getSellIn() < 0) {
-                if (items[i].getName() != "Aged Brie") {
-                    if (items[i].getName() != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].getQuality() > 0) {
-                            if (items[i].getName() != "Sulfuras, Hand of Ragnaros") {
-                                items[i].setQuality(items[i].getQuality() - 1);
-                            }
-                        }
-                    } else {
-                        items[i].setQuality(items[i].getQuality() - items[i].getQuality());
-                    }
-                } else {
-                    if (items[i].getQuality() < 50) {
-                        items[i].setQuality(items[i].getQuality() + 1);
-                    }
-                }
-            }
-        }
-    }
+			if (!isItemSulfuras(itemToUpdate)) {
+				decrementItemSellInByOne(itemToUpdate);
+			}
+
+			if (isItemSellInLessThanZero(itemToUpdate)) {
+				if (!isItemAgedBrie(itemToUpdate)) {
+					if (!isItemConcertPasses(itemToUpdate)) {
+						if (isItemQualityGreaterThanZero(itemToUpdate)) {
+							if (!isItemSulfuras(itemToUpdate)) {
+								decrementItemQualityByOne(itemToUpdate);
+							}
+						}
+					}
+					else {
+						setItemQualityToZero(itemToUpdate);
+					}
+				}
+				else {
+					if (isItemQualityLessThanFifty(itemToUpdate)) {
+						incrementItemQualityByOne(itemToUpdate);
+					}
+				}
+			}
+		}
+	}
+
+	/* TODO: ver si puedo meter estos métodos en una clase */
+	private boolean isItemAgedBrie(Item item) {
+		return item.getName() == NAME_BRIE;
+	}
+
+	private boolean isItemSulfuras(Item item) {
+		return item.getName() == NAME_SULFURAS;
+	}
+
+	private boolean isItemConcertPasses(Item item) {
+		return item.getName() == NAME_BACKSTAGE;
+	}
+
+	private boolean isItemQualityGreaterThanZero(Item item) {
+		return item.getQuality() > 0;
+	}
+
+	private boolean isItemQualityLessThanFifty(Item item) {
+		return item.getQuality() < 50;
+	}
+
+	private boolean isItemSellInLessThanZero(Item item) {
+		return item.getSellIn() < 0;
+	}
+
+	private boolean isItemSellInLessThanSix(Item item) {
+		return item.getSellIn() < 6;
+	}
+
+	private boolean isItemSellInLessThanEleven(Item item) {
+		return item.getSellIn() < 11;
+	}
+
+	private void incrementItemQualityByOne(Item item) {
+		item.setQuality(item.getQuality() + 1);
+	}
+
+	private void decrementItemQualityByOne(Item item) {
+		item.setQuality(item.getQuality() - 1);
+	}
+
+	private void setItemQualityToZero(Item item) {
+		item.setQuality(0);
+	}
+
+	private void decrementItemSellInByOne(Item item) {
+		item.setSellIn(item.getSellIn() - 1);
+	}
 }
